5365f5c0370eb70d25fb00dc58c8c260817675a8
Thomas Asuncion <avaenthomas@gmail.com>
2022-05-02 00:11:48 -0500

    Add numbers to asteroids

--- Asteroid Arithmetic/Assets/Scripts/GameObjects/EnemyController.cs
+++ Asteroid Arithmetic/Assets/Scripts/GameObjects/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 /// <summary>
 /// Enemy controller script
@@ -13,6 +14,11 @@ public class EnemyController : MonoBehaviour
     public int number;
 
     /// <summary>
+    /// Reference to text on GUI
+    /// </summary>
+    public TextMeshProUGUI displayNumber;
+
+    /// <summary>
     /// Speed of Asteroid flying across screen
     /// </summary>
     public float speed;
@@ -20,7 +26,7 @@ public class EnemyController : MonoBehaviour
     /// <summary>
     /// Speed of which the object is rotating around the z-axis
     /// </summary>
-    public float rotationsPerMinute = 25;
+    public float rotationsPerMinute = 10;
 
     /// <summary>
     /// Position in which the asteroid will start rotating
@@ -45,6 +51,9 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Intialize text
+        displayNumber = FindObjectOfType<TextMeshProUGUI>();
+
         // Find Rigidbody and move asteroid at a constant rate
         rigidbody2D = this.GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(speed, 0);
@@ -65,6 +74,8 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        displayNumber.text = number.ToString();
+
         transform.Rotate(0, 0,  positionZToRotate * rotationsPerMinute * Time.deltaTime);
 
         DestroyObjectOffScreen();
